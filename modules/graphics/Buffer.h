6923c66c5f5cc8f6ce4bb30a9479d470ae32068d
Angelo Carly <angelo.carly@protonmail.com>
2024-04-13 20:14:58 +0200

    Add vma

---
+++ modules/graphics/Buffer.h
@@ -0,0 +1,15 @@
+#ifndef BURN_GRAPHICS_BUFFER_H
+#define BURN_GRAPHICS_BUFFER_H
+
+#include <vulkan/vulkan.h>
+#include <vk_mem_alloc.h>
+
+namespace vkt
+{
+    class Buffer
+	{
+		public:
+	};
+}
+
+#endif
---
+++ modules/graphics/Device.h
@@ -0,0 +1,17 @@
+#ifndef BURN_GRAPHICS_DEVICE_H
+#define BURN_GRAPHICS_DEVICE_H
+
+#include <vk_mem_alloc.h>
+#include <vulkan/vulkan.h>
+
+namespace vkt
+{
+    class Device
+    {
+        public:
+			Device() = default;
+            ~Device() = default;
+    };
+}
+
+#endif
